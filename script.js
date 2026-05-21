7e2caa46c8624f8e7b9b3e0fc29727121797f5b4
pokmadriz <p7k.mood@gmail.com>
2026-05-21 19:10:12 +0200

    JAVA
    JAVA 1 (revisar si esta bien escrito)

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-console.log("BMW Exhibition iniciada");
+const adultos=document.getElementById("hamburguesa");
+const ninos=document.getElementById("ensalada");
+const ancianos=document.getElementById("pizza");
+
+const total=document.getElementById("total");
+
+const buyButton=document.getElementById("buyButton");
+
+
+function calcularTotal(){
+let precioAdulto=25;
+let precioNino=12;
+let precioAnciano=18;
+let totalPrecio=
+
+(adultos.value*precioAdulto)+
+(ninos.value*precioNino)+
+(ancianos.value*precioAnciano);
+
+
+total.textContent="€"+totalPrecio;
+
+
+if(totalPrecio>0){
+buyButton.disabled=false;
+
+}else{
+buyButton.disabled=true;
+
+}
+
+}
+
+adultos.addEventListener("input",calcularTotal);
+ninos.addEventListener("input",calcularTotal);
+ancianos.addEventListener("input",calcularTotal);
+buyButton.addEventListener("click",()=>{
+
+alert(
+"Compra realizada correctamente.\nTotal: "+total.textContent
+);
+
+});
